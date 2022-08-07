4cda081efd477b320649c477852074a729e2ee48
NikolayPisklov <koluandino@gmail.com>
2022-08-07 16:27:27 -0700

    Home page filtering for admin adn some color changes

--- SchoolJournal/Controllers/HomeController.cs
+++ SchoolJournal/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using SchoolJournal.Models;
 using SchoolJournal.ViewModels;
 using System.Linq;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Text.Json;
 
 namespace SchoolJournal.Controllers
 {
@@ -11,10 +13,13 @@ namespace SchoolJournal.Controllers
     {
         private readonly SchoolJournalContext _db;
 
+
         public HomeController(SchoolJournalContext db)
         {
             _db = db;
         }
+
+
         [HttpGet]
         public IActionResult Home(User user) 
         {
@@ -31,6 +36,7 @@ namespace SchoolJournal.Controllers
             else if (HttpContext.Session.GetString("Status") == "Admin")
             {
                 List<JournalListContent> journals = GetJournalListContentForAdmin();
+                SetFiltersViewBags();
                 return View(journals);
             }
             else 
@@ -39,6 +45,36 @@ namespace SchoolJournal.Controllers
                 return RedirectToAction("Authorization", "Authorization");
             }
         }
+        [HttpPost]
+        public IActionResult Home(int? subjectId, int? classRangId)  
+        {
+            List<JournalListContent> journals = GetJournalListContentForAdmin();
+            if (subjectId != null && classRangId == null)
+            {
+                List<JournalListContent> filteredJournals = GetJournalListFilteredBySubject((int)subjectId, journals);
+                SetFiltersViewBags();
+                return View(filteredJournals);
+            }
+            else if (subjectId == null && classRangId != null)
+            {
+                List<JournalListContent> filteredJournals = GetJournalListFilteredByClassRank((int)classRangId, journals);
+                SetFiltersViewBags();
+                return View(filteredJournals);
+            }
+            else if (subjectId != null && classRangId != null)
+            {
+                List<JournalListContent> filteredJournals = GetJournalListFilteredBySubject((int)subjectId, journals);
+                filteredJournals = GetJournalListFilteredByClassRank((int)classRangId, filteredJournals);
+                SetFiltersViewBags();
+                return View(filteredJournals);
+            }
+            else 
+            {
+                SetFiltersViewBags();
+                return View(journals);
+            }
+        }
+
 
         private List<JournalListContent> GetJournalListContentForStudent(int classId) 
         {
@@ -70,6 +106,34 @@ namespace SchoolJournal.Controllers
                     orderby c.Title.Length, c.Title
                     select new JournalListContent
                     { Subject = s, Class = c, Teacher = t }).ToList();
+        }
+        private List<JournalListContent> GetJournalListFilteredBySubject(int subjectId, List<JournalListContent> journals) 
+        {
+            return journals.Where(j => j.Subject.Id == subjectId).ToList();
+        }
+        private List<JournalListContent> GetJournalListFilteredByClassRank(int classRankId, List<JournalListContent> journals)
+        {
+            if (classRankId == 1)
+            {
+                return journals.Where(j => j.Class.Title.Length == 3 
+                    && (DateTime.Now.Year - j.Class.RecruitmentDate.Year) <= 3).ToList();
+            }
+            else if (classRankId == 2)
+            {
+                return journals.Where(j => j.Class.Title.Length == 3
+                    && (DateTime.Now.Year - j.Class.RecruitmentDate.Year) >= 4).ToList();
+            }
+            else
+            {
+                return journals.Where(j => j.Class.Title.Length == 4).ToList();
+            }
+
+        }
+        private void SetFiltersViewBags() 
+        {
+            HomePageFilters filters = new HomePageFilters(_db.Subjects.ToList());
+            ViewBag.SubjectsSelectList = filters.GetSubjectsSelectList();
+            ViewBag.ClassRanksSelectList = filters.GetClassRangsSelecteList();
         }
     }
 }
