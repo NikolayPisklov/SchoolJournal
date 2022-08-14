0356505283040f63e51eaa2ec1f85e0f14771343
NikolayPisklov <koluandino@gmail.com>
2022-08-14 16:19:41 -0700

    Teacher schedule

--- SchoolJournal/Controllers/ScheduleController.cs
+++ SchoolJournal/Controllers/ScheduleController.cs
@@ -15,14 +15,21 @@ namespace SchoolJournal.Controllers
 
         public IActionResult StudentSchedule(int? pageNumber)
         {
-            //User user = JsonSerializer.Deserialize<User>(HttpContext.Session.GetString("UserObject"));
+            User user = JsonSerializer.Deserialize<User>(HttpContext.Session.GetString("UserObject"));
             List<DateTime> schoolYearDays = GetSchoolYearDays();
-            ViewBag.Content = GetScheduleContent(/*user.FkClass*/1);
+            ViewBag.Content = GetScheduleContentForStudent(user.FkClass);
             ViewBag.LessonTimes = _db.LessonTimes.OrderBy(lt => lt.StartTime.Length).ThenBy(lt => lt.StartTime).ToList();
             return View("Schedule", Paging<DateTime>.Create(schoolYearDays, pageNumber ?? _currentPage, 7));
         }
-
-        private List<ScheduleContent> GetScheduleContent(int fkClass) 
+        public IActionResult TeacherSchedule(int? pageNumber) 
+        {
+            User user = JsonSerializer.Deserialize<User>(HttpContext.Session.GetString("UserObject"));
+            List<DateTime> schoolYearDays = GetSchoolYearDays();
+            ViewBag.Content = GetScheduleContentForTeacher(user.Id);
+            ViewBag.LessonTimes = _db.LessonTimes.OrderBy(lt => lt.StartTime.Length).ThenBy(lt => lt.StartTime).ToList();
+            return View("Schedule", Paging<DateTime>.Create(schoolYearDays, pageNumber ?? _currentPage, 7));
+        }
+        private List<ScheduleContent> GetScheduleContentForStudent(int fkClass) 
         {
             return (from j in _db.Journals
                     join l in _db.Lessons on j.Id equals l.FkJournal
@@ -30,6 +37,16 @@ namespace SchoolJournal.Controllers
                     join t in _db.Teachers on j.FkTeacher equals t.Id
                     join c in _db.Classes on j.FkClass equals c.Id
                     where j.FkClass == fkClass && j.FkSchoolYear == SchoolDateTime.GetCurrentYearId(_db)
+                    select new ScheduleContent { Journal = j, Lesson = l, Subject = s, Teacher = t, Class = c }).ToList();
+        }
+        private List<ScheduleContent> GetScheduleContentForTeacher(int teacherId)
+        {
+            return (from j in _db.Journals
+                    join l in _db.Lessons on j.Id equals l.FkJournal
+                    join s in _db.Subjects on j.FkSubject equals s.Id
+                    join t in _db.Teachers on j.FkTeacher equals t.Id
+                    join c in _db.Classes on j.FkClass equals c.Id
+                    where j.FkTeacher == teacherId && j.FkSchoolYear == SchoolDateTime.GetCurrentYearId(_db)
                     select new ScheduleContent { Journal = j, Lesson = l, Subject = s, Teacher = t, Class = c }).ToList();
         }
         private List<DateTime> GetSchoolYearDays() 
