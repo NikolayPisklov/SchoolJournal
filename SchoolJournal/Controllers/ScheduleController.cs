e5ab1ed5c6b66456e8b43807f5bfefd188a48687
NikolayPisklov <koluandino@gmail.com>
2022-08-17 11:28:04 -0700

    Adding lesson

--- SchoolJournal/Controllers/ScheduleController.cs
+++ SchoolJournal/Controllers/ScheduleController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using SchoolJournal.ViewModels;
 using System.Text.Json;
 
@@ -37,11 +38,52 @@ namespace SchoolJournal.Controllers
             ViewBag.FkClass = fkClass;
             return View("Schedule", Paging<DateTime>.Create(schoolYearDays, pageNumber ?? _currentPage, 7));
         }
-        public IActionResult ClassesSchedules() 
+        public IActionResult ClassesSchedules()
         {
             List<Class> classes = _db.Classes.Where(c => c.ReleaseDate > DateTime.Now)
                 .OrderBy(c => c.Title.Length).ThenBy(c => c.Title).ToList();
             return View(classes);
+        }
+        [HttpGet]
+        public IActionResult AddLesson(int fkClass, int fkLessonTime, DateTime lessonDate)
+        {
+            Lesson newLesson = new Lesson();
+            newLesson.FkLessonTime = fkLessonTime;
+            newLesson.Date = lessonDate;
+            ViewBag.FkClass = fkClass;
+            ViewBag.JournalsSelectList = GetJournalJournalsSelectList(fkClass);
+            return View(newLesson);
+        }
+        [HttpPost]
+        public IActionResult AddLesson(Lesson newLesson, int fkClass)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(newLesson);
+                _db.SaveChanges();
+                return RedirectToAction("AdminSchedule", new { _currentPage, fkClass });
+
+            }
+            else
+            {
+                ViewBag.FkClass = fkClass;
+                ViewBag.JournalsSelectList = GetJournalJournalsSelectList(fkClass);
+                return View();
+            }
+        }
+        [HttpGet]
+        public IActionResult EditLesson(Lesson lesson) 
+        {
+            int fkClass = _db.Journals.Where(j => j.Id == lesson.FkJournal).Select(j => j.FkClass).First(); 
+            ViewBag.FkClass = fkClass;
+            ViewBag.JournalsSelectList = GetJournalJournalsSelectList(fkClass);
+            return View(lesson);
+        }
+        [HttpPost]
+        public IActionResult EditLesson(int fkClass, Lesson lesson) 
+        {
+
+            return View();
         }
         private List<ScheduleContent> GetScheduleContentForStudent(int fkClass) 
         {
@@ -75,6 +117,23 @@ namespace SchoolJournal.Controllers
                 days.Add(date);
             }
             return days;
+        }
+        private SelectList GetJournalJournalsSelectList(int fkClass) 
+        {
+            List<ScheduleContent> scheduleContents = (from j in _db.Journals
+                                                      join s in _db.Subjects on j.FkSubject equals s.Id
+                                                      join t in _db.Teachers on j.FkTeacher equals t.Id
+                                                      join c in _db.Classes on j.FkClass equals c.Id
+                                                      where j.FkClass == fkClass && j.FkSchoolYear == SchoolDateTime.GetCurrentYearId(_db)
+                                                      select new ScheduleContent 
+                                                      { Journal = j, Subject = s, Teacher = t, Class = c }).ToList();
+            Dictionary<int, string> journals = new Dictionary<int, string>();
+            foreach (ScheduleContent sc in scheduleContents) 
+            {
+                string pointTitle = $"{sc.Subject.Title} - {sc.Teacher.Surname} {sc.Teacher.Name[0]}. {sc.Teacher.Middlename[0]}.";
+                journals.Add(sc.Journal.Id, pointTitle);
+            }
+            return new SelectList(journals, "Key", "Value"); ;
         }
     }
 }
